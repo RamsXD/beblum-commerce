069bd0e4877dc3e15846645f9cfd2fc378b78047
RamsXD <ramsxd67@gmail.com>
2025-11-11 09:11:42 -0300

    Começando listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 export default function Home() {
   return (
-    <div>
+    <div className="max-w- mx-auto pt-8 px-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 xl:gap-6">
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">1</div>
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">2</div>
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">3</div>
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">4</div>
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">5</div>
+        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">6</div>
+      </div>
       <h1>beblum</h1>
     </div>
   );
