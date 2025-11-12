0771044c0404017eab548b350b166d3f912b5374
RamsXD <ramsxd67@gmail.com>
2025-11-12 16:15:47 -0300

    Adicionando componente de produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-export default function Home() {
+import { ProductType } from "../types/ProductType";
+import Product from "./components/Product";
+
+async function fetchProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await fetchProducts();
+  console.log(products);
   return (
     <div className="max-w- mx-auto pt-8 px-8">
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 xl:gap-6">
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">1</div>
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">2</div>
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">3</div>
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">4</div>
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">5</div>
-        <div className="bg-black p-4 rounded-lg shadow-md text-amber-50">6</div>
+        {products.map((product: ProductType) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
       <h1>beblum</h1>
     </div>
