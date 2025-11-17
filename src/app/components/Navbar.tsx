e90dabeea97631801feb9ebffa12bb21cc87c9d5
RamsXD <ramsxd67@gmail.com>
2025-11-17 18:18:36 -0300

    Estilizando componente Produto com Next Image

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 w-full flex py-2 px-8 justify-between bg-slate-800 text-gray-500 items-center">
+    <nav className="fixed top-0 w-full flex py-2 px-8 z-10 justify-between bg-slate-800 text-gray-500 items-center">
       <Link href="/" className="font-bold h-12 flex items-center">
         BeBlum
       </Link>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
@@ -26,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
         <Navbar />
-        <main className="bg-amber-50 h-screen p-16">{children}</main>
+        <main className="h-screen p-16">{children}</main>
       </body>
     </html>
   );
